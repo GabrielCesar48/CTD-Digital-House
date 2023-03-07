7939c39734dd589c29e6979e8edcfa9a9e9cf267
Gabriel <gabrielcesar48@gmail.com>
2023-03-07 07:45:06 -0300

    eventos de teclado

--- frontEnd2/FrontEnd-II/Aula8/A8A/js/script.js
+++ frontEnd2/FrontEnd-II/Aula8/A8A/js/script.js
@@ -1,19 +1,50 @@
-let btMudaCor = document.querySelectorAll('.bt-muda-cor')
+// let btMudaCor = document.querySelectorAll('.bt-muda-cor')
 
-for (let i = 0; i < btMudaCor.length; i++) {
+// for (let i = 0; i < btMudaCor.length; i++) {
 
-    btMudaCor[i].addEventListener('click', function() {
+//     btMudaCor[i].addEventListener('click', function() {
 
-        this.style.color = prompt('Escolha a cor do texto');
-        this.style.background = prompt('Escolha a cor do texto');
+//         this.style.color = prompt('Escolha a cor do texto');
+//         this.style.background = prompt('Escolha a cor do texto');
         
-    })
+//     })
     
-}
+// }
 
 
-let titulo = document.querySelector('#titulo')
+// let titulo = document.querySelector('#titulo')
 
-titulo.addEventListener('mouseover', function () {
-    console.log('voce passou o mouse')
+// titulo.addEventListener('mouseover', function () {
+//     console.log('voce passou o mouse')
+// })
+
+
+// apertar a tecla (keydown) com uma função
+
+document.addEventListener('keydown', function (apertar) {
+    if (apertar.key === 'q') {
+        console.log('Apertei a tecla q')
+    }
+})
+
+
+// SOLTAR A TECLA (KEYUP) COM UMA FUNÇÃO
+
+document.addEventListener('keyup', function(soltar) {
+    if (soltar.key === 'w') {
+        console.log('soltou a tecla w')
+        
+    }
+})
+
+
+const teclado = document.querySelector('.teclado')
+
+teclado.addEventListener('keydown', e => {
+    if (e.key === 'r') {
+
+        alert('opa! você nao pode usar a letra R')
+        e.preventDefault()
+        
+    }
 })
